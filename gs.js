1da2ce47f590ddaad9af9e39a8838caec2003ed6
Davide Pirola <ziocleto@gmail.com>
2020-02-09 03:21:57 +0000

    fixing do

--- gs.js
+++ gs.js
@@ -71,7 +71,7 @@ app.put('/:username/:projecturl', async (req, res) => {
       url: 'https://api.digitalocean.com' + path,
       port: 443,
       path: path,
-      method: "PUT",
+      method: "GET",
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0',
         'Authorization': `Bearer ${process.env.DO_TOKEN}`,
