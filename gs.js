2439772d0fd9d54ef243eae8163f56c9f674d578
Davide Pirola <ziocleto@gmail.com>
2020-02-10 11:14:00 +0000

    adding some sleep, to be on the safer-side

--- gs.js
+++ gs.js
@@ -343,6 +343,8 @@ echo -e "
 cd /docker/letsencrypt-docker-nginx/src/letsencrypt
 sudo docker-compose up -d
 
+sleep 15
+docker ps > /dockerps.log
 sudo docker run -it --rm -v /docker-volumes/etc/letsencrypt:/etc/letsencrypt -v /docker-volumes/var/lib/letsencrypt:/var/lib/letsencrypt -v /docker/letsencrypt-docker-nginx/src/letsencrypt/letsencrypt-site:/data/letsencrypt -v "/docker-volumes/var/log/letsencrypt:/var/log/letsencrypt" certbot/certbot certonly --webroot --register-unsafely-without-email --agree-tos --webroot-path=/data/letsencrypt --staging -d ${projecturl} -d www.${projecturl} > /sslstaging.log
 
 sudo rm -rf /docker-volumes/
